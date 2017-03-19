cedc4b3f54c4ff239e65ed0fec514a0f2202bfd3
Gopesh Raval <graval77@gmail.com>
2017-03-19 16:59:19 -0400

    first commit

--- data.js
+++ data.js
@@ -1,84 +1,156 @@
 var MongoClient = require('mongodb').MongoClient,
- settings = require('./config.js'),
- Guid = require('Guid');
+settings = require('./config.js'),
+Guid = require('Guid');
 
 var fullMongoUrl = settings.mongoConfig.serverUrl + settings.mongoConfig.database;
 var exports = module.exports = {};
 
 MongoClient.connect(fullMongoUrl)
-    .then(function(db) {
-        var myCollection = db.collection("user_collection");
-        
-        // setup your exports!
-        
-        exports.getUser = function(_username){          
-            return myCollection.find({ username: _username }).limit(1).toArray().then(function(user) {
-                if (user.length === 0) return Promise.reject("Username : \'" +_username +"\' not found!");
-                return user[0];
+.then(function(db) {
+    var myCollection = db.collection("user_collection");
+    var myApplicationCollection = db.collection("application_collection");
+    
+    // setup your exports!
+    
+    exports.getUser = function(_username){          
+        return myCollection.find({ username: _username }).limit(1).toArray().then(function(user) {
+            if (user.length === 0) return Promise.reject("Username : \'" +_username +"\' not found!");
+            return user[0];
+        });
+    };
+
+    exports.getUserFromID = function(id){            
+        return myCollection.find({ _id: id }).limit(1).toArray().then(function(user) {
+            if (user.length === 0) return Promise.reject("No user found");
+            return user[0];
+        });
+    };
+
+    exports.getUserFromSessionID = function(sessionID){            
+        return myCollection.find({ currentSessionId: sessionID }).limit(1).toArray().then(function(user) {
+            if (user.length === 0) return Promise.reject("No user found");
+            return user[0];
+        });
+    };
+
+    exports.createUser = function(_username, pass_hash){        
+        var user = {
+            _id : Guid.create().toString(),
+            username: _username,
+            encryptedPassword: pass_hash,
+            currentSessionId: '',
+            user_status:'',
+            last_visited:'',
+            profile: {
+                firstName: '',
+                lastName: '',
+                emailId: '',
+                photo: '',
+                resume:'',
+                education:'',
+                skills:'',
+                experience:''
+            },
+        };
+
+        return myCollection.find({ username: _username }).limit(1).toArray().then(function(user_found) {
+            if (user_found.length !== 0) 
+            return Promise.reject("Username : \'" +_username +"\' already exists! Try a different username!");
+            
+            return myCollection.insertOne(user).then(function (newUser) {
+                return newUser.insertedId;
+            }).then(function(user_id){
+                return exports.getUserFromID(user_id)
             });
-        };
+        });
 
-        exports.getUserFromID = function(id){            
-            return myCollection.find({ _id: id }).limit(1).toArray().then(function(user) {
-                if (user.length === 0) return Promise.reject("No user found");
-                return user[0];
+    };
+    exports.userExists = function(uname){
+            console.log("in userExists");
+            if(!uname) return Promise.reject("Missing Uname");
+            
+            return myCollection.find({'username': uname}).limit(1).toArray().then(function(userList){
+                console.log("userlist length"+userList.length)
+                if(userList.length === 0)
+                    {
+                        console.log("no user");
+                        return true;
+                    } 
+                else return Promise.reject("Username already taken.");
             });
-        };
+    };
+    exports.updateUser = function(_sessionID, _profile){            
+        return myCollection.updateOne({ currentSessionId: _sessionID }, { $set: { "profile": _profile } }).then(function() {
+            return exports.getUserFromSessionID(_sessionID);
+        });
+    };
 
-        exports.getUserFromSessionID = function(sessionID){            
-            return myCollection.find({ currentSessionId: sessionID }).limit(1).toArray().then(function(user) {
-                if (user.length === 0) return Promise.reject("No user found");
-                return user[0];
-            });
-        };
+    exports.insertSessionID = function(_userID, sessionID){            
+        return myCollection.updateOne({ _id: _userID }, { $set: { "currentSessionId": sessionID } }).then(function() {
+            return exports.getUserFromID(_userID);
+        });
+    };
 
-        exports.createUser = function(_username, pass_hash){        
-            var user = {
-                _id : Guid.create().toString(),
-                username: _username,
-                encryptedPassword: pass_hash,
-                currentSessionId: '',
-                user_status:'',
-                last_visited:'',
-                profile: {
-                    firstName: '',
-                    lastName: '',
-                    emailId: '',
-                    photo: '',
-                    resume:'',
-                    education:'',
-                    skills:'',
-                    experience:''
-                }
-            };
+    exports.clearSessionID = function(sessionID){           
+        return myCollection.updateOne({ currentSessionId: sessionID }, { $set: { "currentSessionId": "" } });
+    };
+    //Shristi
+    exports.getApplication = function(id) {
+        if (id === undefined) return Promise.reject("You must provide an ID");
 
-            return myCollection.find({ username: _username }).limit(1).toArray().then(function(user_found) {
-                if (user_found.length !== 0) 
-                return Promise.reject("Username : \'" +_username +"\' already exists! Try a different username!");
-                
-                return myCollection.insertOne(user).then(function (newUser) {
-                    return newUser.insertedId;
-                }).then(function(user_id){
-                    return exports.getUserFromID(user_id)
-                });
-            });
+        return myApplicationCollection.find({ _id: id }).limit(1).toArray().then(function(listOfApplications) {
+            if (listOfApplications.length === 0) throw "Could not find application with id of " + id;
+            return listOfApplications[0];
+        });
+    };
+    exports.insertForm_forUser = function(_username,_currentSession,_company,_contact,_email,_appliedOn,_status,_site,_skillsRequired,_note,_followUpOn,_remind)
+    {   var newApplication =    {
+                                    _id: Guid.create().toString(),
+                                    "creator":_username,
+                                    "company":_company,
+                                    "contact":_contact,
+                                    "email":_email,
+                                    "appliedOn": new Date(_appliedOn),
+                                    "status":_status,
+                                    "site":_site,
+                                    "skillsRequired":_skillsRequired,
+                                    "note":_note,
+                                    "followUpOn":new Date(_followUpOn),
+                                    "remind":new Date(_remind)
+                                }
+        return myApplicationCollection.insertOne(newApplication).then(function (newApplication) {
+                return newApplication.insertedId;
+        });
+    };
 
-        };
+    exports.getUserByCredentials = function(username, password){
+        console.log("in getUserByCredentials, username="+username+" ,password="+password);
+        if(!username) return Promise.reject("You must provide an Username");
+        if(!password) return Promise.reject("You must provide an password");
 
-        exports.updateUser = function(_sessionID, _profile){            
-            return myCollection.updateOne({ currentSessionId: _sessionID }, { $set: { "profile": _profile } }).then(function() {
-                return exports.getUserFromSessionID(_sessionID);
-            });
-        };
+        return myCollection.find({"username": username}).limit(1).toArray().then(function(listOfUsers){
+            console.log("listOfUsers.length= "+listOfUsers.length);
+            if(listOfUsers.length === 0) {
+                console.log("could not find user with the username of "+Username);
+                return Promise.reject('could not find user with the username of '+username);
+            }
 
-        exports.insertSessionID = function(_userID, sessionID){            
-            return myCollection.updateOne({ _id: _userID }, { $set: { "currentSessionId": sessionID } }).then(function() {
-                return exports.getUserFromID(_userID);
-            });
-        };
+            return listOfUsers[0];
+        });
+    };
+        //Shristi
+    exports.getApplications = function(username) {
+        if (username === undefined)
+        {   
+            console.log("error")
+            return Promise.reject("You must provide an Username");
+        } 
 
-        exports.clearSessionID = function(sessionID){           
-            return myCollection.updateOne({ currentSessionId: sessionID }, { $set: { "currentSessionId": "" } });
-        };
-        
-    });
+        return  myApplicationCollection.find({ "creator" : username }).toArray().then(function(applicationList) {
+            console.log('application'+applicationList.length);
+            return applicationList;
+        });
+    }
+
+    
+});
--- server.js
+++ server.js
@@ -6,13 +6,31 @@ var expressEjsLayouts = require('express-ejs-layouts');
 var Guid = require('Guid');
 var bcrypt = require("bcrypt-nodejs");
 var cookieParser = require('cookie-parser');
+var path = require('path');
+var nodemailer = require('nodemailer');
+var schedule = require('node-schedule');
+
 // This package exports the function to create an express instance:
 var app = express();
+var multer  =   require('multer');
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+
+var upload = multer({ storage : storage}).single('userPhoto');
+var router = express.Router();
+//app.use('/sayHello', router);
 
 // We can setup Jade now!
 app.set('view engine', 'ejs');
 app.set('view options', { layout:'layout.ejs' });
 app.use(expressEjsLayouts);
+app.use(cookieParser());
 // This is called 'adding middleware', or things that will help parse your request
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -21,8 +39,50 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 // any path starting with /assets;
 // it will check the 'static' folder for matching files 
 app.use('/assets', express.static('static'));
+app.use(function(request, response, next){
+    console.log("request.cookies.currentSessionId = "+request.cookies.currentSessionId);
+    if(!request.cookies.currentSessionId){
+        console.log("before creating session cookie");
+        var expiresAt = new Date();
+        expiresAt.setMinutes(expiresAt.getHours() + 1);
+        var session = Guid.create().toString();
+        response.cookie("currentSessionId", session, {expires: expiresAt});
+        app.locals.userSession = request.cookies.currentSessionId;
+    }
+    next();
+});
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(1, 6)];
+rule.hour = 9;
+rule.minute =30;
+//rule.minute = new schedule.Range(0, 59, 2);
 
-// Setup your routes here!
+schedule.scheduleJob(rule, function(){
+    console.log(rule);
+    console.log('Today is recognized by Rebecca Black!---------------------------');
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'paperless.cs546@gmail.com', // Your email id
+            pass: 'paperlessstevens' // Your password
+        }
+    });
+    var mailOptions = {
+        from: 'paperless.cs546@gmail.com', // sender address
+        to: 'shingle@stevens.edu', // list of receivers
+        subject: 'Email Example', // Subject line
+        text: "hello" //, // plaintext body
+        // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Message sent: ' + info.response);
+        };
+    });
+
+});
 //GET methods 
 app.get("/", function (request, response) { 
     response.render("pages/index",{layout: false});
@@ -30,9 +90,52 @@ app.get("/", function (request, response) {
 app.get("/dashboard", function (request, response) { 
     response.render("pages/dashboard",{ layout: true, choice: 'dashboard'});
   });
-app.get("/forms", function (request, response) { 
-    response.render("pages/forms",{ layout: true, choice: 'forms'});
+
+//Pooja Added
+app.post("/forms", function (request, response){
+   
+    var currentSessionId = app.locals.userSession;
+    console.log(request.body.company);
+    console.log(request.body.contact);
+    console.log(request.body.email);
+    console.log(request.body.appliedOn);
+    console.log(request.body.status);
+    console.log(request.body.skills);
+    console.log(request.body.username);
+    console.log(request.body.followOn);
+    console.log(request.body.reminder);
+    console.log(request.body.note);
+
+    myData.insertForm_forUser(request.body.username,request.body.currentSessionId,request.body.company,request.body.contact,request.body.email,request.body.appliedOn,request.body.status,request.body.skills,request.body.note,request.body.followOn,request.body.reminder).then(function(user) {
+        //response.json(user);
+        response.render("pages/forms",{ layout: true, choice: 'forms'});
+    }, function(errorMessage) {
+        response.status(500).json({ error: errorMessage });
+    });
 });
+
+//pooja Added
+app.get("/forms", function (request, response) { 
+    
+    var currentSessionId = app.locals.userSession;
+    var company = request.query.company;
+    var contact = request.body.contact;
+    var email = request.body.email;
+    var appliedOn = request.body.appliedOn;
+    var status = request.body.status;
+    var skills = request.body.skills; 
+    var note = request.body.note;
+    var reminder = request.body.reminder;
+    var followOn = request.body.followOn;
+
+    
+    myData.insertForm_forUser(currentSessionId,company,contact,email,appliedOn,status,skills,note,reminder,followOn).then(function(user) {
+        //response.json(user);
+        response.render("pages/forms",{ layout: true, choice: 'forms'});
+    }, function(errorMessage) {
+        response.status(500).json({ error: errorMessage });
+    });
+}); 
 app.get("/tables", function (request, response) { 
     response.render("pages/tables",{layout: true, choice: 'tables'});
 });
@@ -46,74 +149,74 @@ app.post("/login", function (request, response) {
    myData.getUser(request.body.username).then(function(user) {
         bcrypt.compare(request.body.pass, user.encryptedPassword, function (err, result) {
             if (result === true) {
+                console.log("Passwords matches the hash, user id = "+user._id);
+                
+                console.log("before creating session cookie");
                 var expiresAt = new Date();
                 expiresAt.setHours(expiresAt.getHours() + 1);
-                var generatedSessionID = Guid.create().toString();
-                response.cookie("_currentSessionId", generatedSessionID, { expires: expiresAt });
-                myData.insertSessionID(user._id,generatedSessionID).then(function(updated){
+                response.cookie("currentSessionId", user.currentSessionId, {expires: expiresAt});
+                response.locals.userSession = request.cookies.currentSessionId;
+                myData.insertSessionID(user._id,user.currentSessionId).then(function(updated){
                 });
+                app.locals.username = request.body.username;
                 response.render("pages/profile", { layout: true, choice: 'profile' });
             } else {
-                response.render("pages/index", { pageTitle: "User Login and Profile system", _error: "Passwords do not match!" });
+                response.render("pages/index", {layout: false, pageTitle: "User Login and Profile system", _error: "Passwords do not match!" });
             }
         });
 
     }, function(errorMessage) {
-        response.render("pages/index", { pageTitle: "User Login and Profile system", _error: errorMessage });
+        response.render("pages/index", { layout: false, pageTitle: "User Login and Profile system", _error: errorMessage });
     });
 });
 
 
-app.use('/login', function(request, response, next){
-    next();
-});
-
-
-app.use("/signup", function (request, response,next) { 
+app.post("/signup", function (request, response,next) { 
     var hash = bcrypt.hashSync(request.body.pass);
-
-    myData.createUser(request.body.username, hash, Guid.create().toString()).then(function(inserted_user) {
-        var expiresAt = new Date();
-        expiresAt.setHours(expiresAt.getHours() + 1);
-        var generatedSessionID = Guid.create().toString();
-        response.cookie("_currentSessionId", generatedSessionID, { expires: expiresAt });
-
-        myData.insertSessionID(inserted_user._id,generatedSessionID).then(function(updated){
+    var usrnm = request.body.username;
+    myData.userExists(usrnm).then(function(){
+        console.log(request.cookies.currentSessionId);
+        myData.createUser(usrnm, hash, Guid.create().toString()).then(function(user){
+            console.log("New User="+user.username);
+            myData.insertSessionID(user._id,request.cookies.currentSessionId).then(function(updated){
+            });
+            app.locals.username = request.body.username;
+            response.render("pages/dashboard", { layout: true, choice: 'dashboard' });
+        }, function(errorMessage){
+            response.render("pages/index",{ layout: false, pageTitle: "User Login and Profile system", _error: errorMessage });
         });
-        response.render("pages/profile", { layout: true, choice: 'profile' });
-    }, function(errorMessage) {
-        response.render("pages/index", { pageTitle: "User Login and Profile system", _error: errorMessage });
+
+        //else render error on home page
+    }, function(errorMessage){
+         response.render("pages/index", { pageTitle: "User Login and Profile system", _error: errorMessage, layout: false });
     });
 });
 
 
-app.post('/signup', function(request, response){
-    console.log("UserName : " + request.body.username);
-    console.log("Password : " + request.body.pass);
-    console.log("Checking : " + response.locals.dat);
-});
-
-
-app.use('/logout', function(request, response){
+app.all("/logout", function(request, response){
     console.log("now clearing the cookie");
-
-    myData.clearSessionID(request.cookies._currentSessionId);
-    var anHourAgo = new Date();
-    anHourAgo.setHours(anHourAgo.getHours() -1);
-
-    // invalidate, then clear so that lastAccessed no longer shows up on the
-    // cookie object
-    response.cookie("_currentSessionId", "", { expires: anHourAgo });
-    response.clearCookie("_currentSessionId");
-
-    response.render("pages/index", { layout:false });
+        
+    var expiresAt = new Date();
+    expiresAt.setHours(expiresAt.getHours() -1);
+    response.cookie("currentSessionId", "", { expires: expiresAt });
+    response.clearCookie("currentSessionId");
+    app.locals.username = undefined;
+    response.locals.userSession = undefined;
+    console.log("Cookies cleared, redirecting to Home");
+    response.redirect("/");
 });
 
-
-app.post('/logout', function(request, response){
-    console.log("In Logout : ");
+app.post('/photo',function(request,response){
+    upload(request,response,function(err) {
+        if(err) {
+            //return response.end("Error uploading file.");
+            console.log(err);
+        }
+        //path.join(__dirname, 'path/to/file')
+        //res.render("pages/profile", { root: path.join(__dirname, '../public') });
+        response.render("pages/profile",{layout: true, choice: 'profile'});
+        });
 });
-
 // We can now navigate to localhost:3000
 app.listen(3000, function () {
     console.log('Your server is now listening on port 3000! Navigate to http://localhost:3000 to access it');
